24aee5d5c1e1955f404c430149f9ec8879cb805b
Varakorn Kaewmanee <varakorn2324@gmail.com>
2026-02-24 15:07:54 +0700

    shitty nav

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,9 +6,12 @@ import Link from "next/link";
 
 export default ()=>{
     return (
-        <div className="h-fit w-full flex items-center bg-black py-2 px-3">
+        <div className="h-fit w-full flex items-center bg-black py-2 px-6 justify-between">
             <Link href={"/"}>
                 <AiOutlineHome className="w-10 h-10 text-white"/>
+            </Link>
+            <Link href={"/dashboard"}>
+                <p className="font-semibold text-base">Dashboad</p>
             </Link>
         </div>
     )
